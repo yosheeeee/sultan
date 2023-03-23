3c356cce939fb5643c90d9c7ae422783950454ce
yosheeeee <evantipin@edu.hse.com>
2023-03-23 16:50:54 +0500

    add desktop footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -7,7 +7,77 @@ import './style/style.css'
 const Footer = () => {
     return (  
         <footer>
-        <div className="footer--contet">
+        <div className="footer--content">
+            <img src="img/footer/logo.svg" alt="logo" className="logo" />
+            <p className="about-company">
+                Компания «Султан» — снабжаем розничные магазины товарами
+                "под ключ" в Кокчетаве и Акмолинской области
+            </p>
+            <p className="subscribe">
+                Подпишись на скидки и акции
+            </p>
+
+            <form name="inputEmail">
+                <input type="email" name="inputEmailText" placeholder="Введите ваш E-mail" />
+                <input type="submit" value="" name="sendEmail" />
+            </form>
+
+            <div className="menu">
+                <h2>Меню сайта:</h2>
+                <ul>
+                    <li>
+                        О компании
+                    </li>
+                    <li>Доставка и оплата</li>
+                    <li>Возврат</li>
+                    <li>Контакты</li>
+                </ul>
+            </div>
+
+            <div className="categories">
+                <h2>Категории</h2>
+                <ul>
+                    <li>Бытовая химия</li>
+                    <li>Косметика и гигиена</li>
+                    <li>Товары для дома</li>
+                    <li>Товары для детей и мам</li>
+                    <li>Посуда</li>
+                </ul>
+            </div>
+
+            <div className="price-list">
+                <h2>Скачать прайс-лист:</h2>
+                <button>
+                    <p>Прайс-лист</p>
+                    <img src="img/footer/download.svg" alt="download" />
+                </button>
+
+                <div className="price-list--links">
+                    <p>Связь в мессенджерах:</p>
+                    <ul>
+                        <li className="whatsapp" />
+                        <li className="telegram" />
+                    </ul>
+                </div>
+            </div>
+
+            <div className="contacts">
+                <h2>Контакты:</h2>
+                <strong>+7 (777) 490-00-91</strong>
+                <p>время работы: 9:00-20:00</p>
+                <p><span>Заказать звонок</span></p>
+
+                <p className="mail">
+                    <span>opt.sultan@mail.ru </span> <br />
+                    На связи в любое время
+                </p>
+
+                <ul>
+                    <li className="visa" />
+                    <li className="mastercard" />
+                </ul>
+            </div>
+
         </div>
         <div className="footer--content--mobile">
             <img src="img/footer/logo-mobile.svg" alt="logo" className="footer--content--mobile--logo" />
